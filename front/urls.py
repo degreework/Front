d1c554ab636b72402a171e77b7d178c495135df5
donberna <donberna-93@hotmail.com>
2015-05-22 20:02:36 -0500

    login and register js

--- front/urls.py
+++ front/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('front_users.urls', namespace='front_users')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
