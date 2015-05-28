43e8c9a126d2a7bfbcf87cdb77764f5c93bc0196
rizotas <aslheyramirez@gmail.com>
2015-05-28 11:41:07 -0500

    refactoring isAutenticated js

--- front_users/static/js/account/session_manager.js
+++ front_users/static/js/account/session_manager.js
@@ -9,65 +9,72 @@ function showCurrentUser()
 	$("#user_resume").show();	
 }
 
-
-function isAutenticated () {
-
-	$.ajax({
-		type: 'GET',
-		url: "http://127.0.0.1:8080/API/users/me",
-		beforeSend : function( xhr ) {
-        	xhr.setRequestHeader( "Authorization", "Bearer " + $.cookie("Token") );
-    	}
-	})
-	.done(function(response){
-		/*
-		Login succesful, then do anything
-		*/
-
-		console.log(response)
-		$.cookie("username", response.first_name+" "+response.last_name)
+function isAutenticated()
+{
+	if($.cookie("username"))
+	{
 		showCurrentUser();
+	}
+}
 
-	})
-	.fail(function(error){		
-		console.log(error);
-		//if status ==0  -> can't connect to server
-		if(0 == error.status)
-		{
-			console.log("can't connect to server")
-		}
+function getCurrentUser () {
 
-		//if BAD REQUEST -> show error response in fields form
-		if(400 == error.status || 401 == error.status)
-		{
-			
-		}
-		/*
-		// if UNAUTHORIZED ->
-		else if(401 == error.status)
-		{
-			//create new div for error message
-			div = document.createElement("div");
-			
-			//set id to div
-			div.id = "error_login";
-			
-			//set class to div
-			//div.className = 
+		$.ajax({
+			type: 'GET',
+			url: "http://127.0.0.1:8080/API/users/me",
+			beforeSend : function( xhr ) {
+	        	xhr.setRequestHeader( "Authorization", "Bearer " + $.cookie("Token") );
+	    	}
+		})
+		.done(function(response){
+			/*
+			Login succesful, then do anything
+			*/
 
-			//append error message to div
-			div.appendChild(document.createTextNode(error.responseJSON.error_description));
-			$("#form_login").prepend(div);	
-		}*/
-		//if INTERNAL SERVER ERROR
-		else if(500 == error.status)
-		{
-			//if url is incorret
-		}
-	})
-	.always(function(){
-		console.log("always");
-	});
-	
+			console.log(response)
+			$.cookie("username", response.first_name+" "+response.last_name)
+			showCurrentUser();
+
+		})
+		.fail(function(error){		
+			console.log(error);
+			//if status ==0  -> can't connect to server
+			if(0 == error.status)
+			{
+				console.log("can't connect to server")
+			}
+
+			//if BAD REQUEST -> show error response in fields form
+			if(400 == error.status || 401 == error.status)
+			{
+				
+			}
+			/*
+			// if UNAUTHORIZED ->
+			else if(401 == error.status)
+			{
+				//create new div for error message
+				div = document.createElement("div");
+				
+				//set id to div
+				div.id = "error_login";
+				
+				//set class to div
+				//div.className = 
+
+				//append error message to div
+				div.appendChild(document.createTextNode(error.responseJSON.error_description));
+				$("#form_login").prepend(div);	
+			}*/
+			//if INTERNAL SERVER ERROR
+			else if(500 == error.status)
+			{
+				//if url is incorret
+			}
+		})
+		.always(function(){
+			console.log("always");
+		});
+
 }
 
--- front_users/static/js/forms/form_login.js
+++ front_users/static/js/forms/form_login.js
@@ -14,6 +14,7 @@ function login(form, url)
 
 		console.log(response)
 		$.cookie("Token", response.access_token);
+		getCurrentUser();
 		isAutenticated();
 
 	})
