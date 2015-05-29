ee15672d699eac6e5802b21787198cc825fdb384
donberna <donberna-93@hotmail.com>
2015-05-29 18:44:22 -0500

    settings view

---
+++ front_users/static/js/forms/form_settings.js
@@ -0,0 +1,81 @@
+function info_user_settings()
+{
+	
+
+	$.ajax({
+		type: 'GET',
+		url: "http://127.0.0.1:8080/API/users/"+jQuery.parseJSON($.cookie("username")).id+'/',
+		beforeSend : function( xhr ) {
+	        	xhr.setRequestHeader( "Authorization", $.cookie("Token").token_type +" "+ $.cookie("Token").access_token );
+	    	}
+
+	})
+	.done(function(response){
+		
+		$('#id_password').remove();	
+		//fill the information of user in the input 
+		$.each(response, function(key, value) {			
+
+			$('#id_'+key).attr('placeholder', $('#id_'+key).attr('placeholder') +': '+value);
+
+			if('id_'+key === 'id_plan'){
+				$('#id_'+key).val(value); 			
+			}
+
+		});
+
+
+	})
+	.fail(function(error){		
+		console.log(error);
+		//if status ==0  -> can't connect to server
+		if(0 === error.status)
+		{
+			console.log("can't connect to server")
+		}
+
+		//if BAD REQUEST -> show error response in fields form
+		if(400 == error.status || 401 == error.status)
+		{
+			//create new div for error message
+			div = document.createElement("div");
+			
+			//set id to div
+			div.id = "error_login";
+			
+			//set class to div
+			//div.className = 
+
+			//append error message to div
+			div.appendChild(document.createTextNode(error.responseJSON.error_description));
+			$("#form_login").prepend(div);
+		}
+		/*
+		// if UNAUTHORIZED ->
+		else if(401 == error.status)
+		{
+			//create new div for error message
+			div = document.createElement("div");
+			
+			//set id to div
+			div.id = "error_login";
+			
+			//set class to div
+			//div.className = 
+
+			//append error message to div
+			div.appendChild(document.createTextNode(error.responseJSON.error_description));
+			$("#form_login").prepend(div);	
+		}*/
+		//if INTERNAL SERVER ERROR
+		else if(500 == error.status)
+		{
+			//if url is incorret
+		}
+	})
+	.always(function(){
+		console.log("always");
+	});
+
+}
+
