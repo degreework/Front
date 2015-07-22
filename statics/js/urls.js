40d8621652f7042997ed408f672c742b1b0d8749
rizotas <aslheyramirez@gmail.com>
2015-07-21 19:47:41 -0500

    Fuck you password restore

--- statics/js/urls.js
+++ statics/js/urls.js
@@ -16,6 +16,7 @@ URL_UPDATE_USER = host+":8080/API/users/";
 URL_UPDATE_USER_PASSWORD = host+":8080/API/users/authenticate/";
 URL_BRING_ALL_USERS = host+":8080/API/users/all";
 URL_DETAIL_USERS = host+":8080/API/users/detail/";
+URL_RECOVERY_PASSWORD = host+":8080/API/users/recovery";
 
 //Degree == Plan
 URL_GET_DEGREE = host+":8080/API/degree/all";
