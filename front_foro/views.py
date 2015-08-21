f660d638c38c289f693e8697c960231b98a77449
rizotas <aslheyramirez@gmail.com>
2015-08-21 18:25:59 -0500

    fix something

--- front_foro/views.py
+++ front_foro/views.py
@@ -3,18 +3,21 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
-# Create your views here.
+from decorators.ScopeRequired import ScopeRequired
+
+
+@ScopeRequired(["forum.can_view"])
 def foro(request):
     return render_to_response('foroList.html', {'title': 'foro | Name App'}, context_instance=RequestContext(request))
 
-
+@ScopeRequired(["forum.can_view"])
 def detail(request,id):
     return render_to_response('askDetail.html', {'title': 'foro | Name App'}, context_instance=RequestContext(request))
 
-
+@ScopeRequired(["forum.add_ask"])
 def create(request):
     return render_to_response('foroCreate.html', {'title': 'Create Foro | Name App'}, context_instance=RequestContext(request))
 
-
+@ScopeRequired(["forum.change_ask"])
 def edit(request):
     return render_to_response('Foroedit.html', {'title': 'Edit Foro | Name App'}, context_instance=RequestContext(request))
