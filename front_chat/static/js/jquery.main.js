adad0d2c8dde02045d81480bab712377f3056d44
rizotas <aslheyramirez@gmail.com>
2015-11-12 23:08:30 -0500

    set url chat theme

--- front_chat/static/js/jquery.main.js
+++ front_chat/static/js/jquery.main.js
@@ -1258,8 +1258,8 @@ $( document ).ready(function() {
 
     //Change theme function
     function main_set_theme( theme ) {
-      var theme_css = "http://127.0.0.1:8000/static/themes/" + theme + "/jquery-ui.min.css";    
-      $( "#theme" ).attr( "href" , theme_css );
+      //var theme_css = "http://127.0.0.1:8000/static/themes/" + theme + "/jquery-ui.min.css";    
+      //$( "#theme" ).attr( "href" , theme_css );
       return false;
     }
 
